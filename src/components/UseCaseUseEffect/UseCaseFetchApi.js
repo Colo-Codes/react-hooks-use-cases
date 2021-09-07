cfee471cf38612baabe5850886c75291994bc925
Damian Demasi <damian.demasi.1@gmail.com>
2021-09-08 09:09:35 +0930

    Spinner added

--- src/components/UseCaseUseEffect/UseCaseFetchApi.js
+++ src/components/UseCaseUseEffect/UseCaseFetchApi.js
@@ -1,16 +1,19 @@
 import { useState, useEffect } from "react";
 
 const UseCaseFetchApi = props => {
-    // useState is needed in order to display the result on the screen
+    // useState is needed in order to display the result and spinner on the screen
     const [bio, setBio] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
 
     // 'async' shouldn't be used in the useEffect callback function because these callbacks are synchronous to prevent race conditions. We need to put the async function inside.
     useEffect(() => {
         const fetchData = async () => {
+            setIsLoading(true);
             const response = await fetch('https://swapi.dev/api/people/1/');
             const data = await response.json();
             console.log(data);
             setBio(data);
+            setIsLoading(false);
         };
         fetchData();
     }, []);
@@ -22,7 +25,7 @@ const UseCaseFetchApi = props => {
             <h2>useEffect use case</h2>
             <h3>Running once on mount: fetch API data</h3>
             <p>Luke Skywalker's bio:</p>
-            <pre>{JSON.stringify(bio, null, '\t')}</pre>
+            {isLoading ? <p>Getting data... 👤</p> : <pre>{JSON.stringify(bio, null, '\t')}</pre>}
         </>
     );
 };
