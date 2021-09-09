189c99876710a2c233513acbac3f518ad67ec174
Damian Demasi <damian.demasi.1@gmail.com>
2021-09-09 11:38:29 +0930

    useEffect use case on updated API

--- src/components/UseCaseUseEffect/UseCaseUpdateFetch.js
+++ src/components/UseCaseUseEffect/UseCaseUpdateFetch.js
@@ -1,64 +1,39 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
-const FetchBacon = props => {
-    const [updatedBacon, setUpdatedBacon] = useState([]);
-    const [childIsLoading, setChildIsLoading] = useState(false);
-
-    // Destructure the props object
-    const { onParagraphNumber, onGetBacon, onIsLoading } = props;
+const BaconParagraphs = props => {
+    const [baconParagraphText, setBaconParagraphText] = useState([]);
 
     useEffect(() => {
-        const getBacon = async (paragraphNumber) => {
-            setChildIsLoading(true);
-            const response = await fetch(`https://baconipsum.com/api/?type=all-meat&paras=${paragraphNumber}&start-with-lorem=1`);
-            const data = await response.json();
-            setChildIsLoading(false);
-            setUpdatedBacon(data);
-        };
-        getBacon(onParagraphNumber);
-    }, [onParagraphNumber]); // Props 
-
-    useEffect(() => {
-        onGetBacon(updatedBacon); // Lifting the state up
-    }, [updatedBacon, onGetBacon]); // Props and local state
-
-    useEffect(() => {
-        onIsLoading(childIsLoading); // Lifting the state up
-    }, [childIsLoading, onIsLoading]); // Props and local state
+        setBaconParagraphText(props.chopBacon.map(piece => <p key={Math.random()}>{piece}</p>));
+    }, [props.chopBacon]); // Props
 
     return (
-        <p>Number of paragraphs: {onParagraphNumber}</p>
+        <>
+            <p>Number of paragraphs: {baconParagraphText.length}</p>
+            {baconParagraphText}
+        </>
     );
-
 };
 
 const UseCaseUpdateFetch = () => {
-    const [paragraphNumber, setParagraphNumber] = useState(1);
     const [bacon, setBacon] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
-    const submitHandler = e => {
+    const submitHandler = async e => {
         e.preventDefault();
-        setParagraphNumber(e.target.paragraphs.value);
-    };
 
-    // This function will receive the 'loading' lifted state from the child component
-    const getIsLoadingHandler = (childIsLoading) => {
-        setIsLoading(childIsLoading);
+        setIsLoading(true);
+        const response = await fetch(`https://baconipsum.com/api/?type=all-meat&paras=${e.target.paragraphs.value}&start-with-lorem=1`);
+        const data = await response.json();
+        setIsLoading(false);
+        setBacon(data);
     };
-
-    // This function will receive the 'paragraphs' lifted state from the child component
-    const getBaconHandler = (fetchedBacon) => {
-        setBacon(fetchedBacon);
-    };
-
-    const baconParagraphText = bacon.map(p => <p key={Math.random()}>{p}</p>);
 
     return (
         <>
             <hr />
             <h2>useEffect use case</h2>
-            <h3>Running on props change: updating fetched API data to get Bacon ipsum paragraphs</h3>
+            <h3>Running on props change: update paragraph list on fetched API data update</h3>
             <form onSubmit={submitHandler}>
                 <label htmlFor="paragraphs" style={{ display: "block", marginBottom: "1rem" }}>How many paragraphs of "Bacon ipsum" do you want?</label>
                 <select id="paragraphs" name="paragraphs">
@@ -69,8 +44,7 @@ const UseCaseUpdateFetch = () => {
                 </select>
                 <input type="submit" value="Show me the bacon!" style={{ marginLeft: "1rem" }} /> {isLoading && <span>Getting paragraphs... 🐷</span>}
             </form>
-            <FetchBacon onParagraphNumber={paragraphNumber} onGetBacon={getBaconHandler} onIsLoading={getIsLoadingHandler} />
-            {baconParagraphText}
+            <BaconParagraphs chopBacon={bacon} />
         </>
     );
 };
